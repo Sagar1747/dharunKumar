e76406fe6693fb62f1d84bb37571228f32926591
ANORA\vidyasagar.s <vidyasagar.s@anorasolutions.com>
2026-03-26 17:52:20 +0530

    Count Function Added/Not working

---
+++ sizeOfArray.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int count(int arr[]){
+
+    int temp ;
+    temp = sizeof(arr)/sizeof(arr[0]);
+    return temp;
+
+}
+int main(){
+    
+}
--- sometginh.c
+++ sometginh.c
@@ -9,12 +9,15 @@ int largest(int arr[],int n){
         
     }
     return temp;
+int count (int arr[]);
 
 
 }
 int main(){
+    
     int arr[] = {45,3,12,67,23,1};
-    int large = largest(arr,6);
+    int n = count(arr);
+    int large = largest(arr,n);
     printf("Largest number: %d",large);
 
 }
