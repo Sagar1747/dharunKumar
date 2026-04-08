c512b3ae0db5188424115fa337f11607be1de3c6
Hemanth Krishna <hemanthkrishna.ab@anorasolutions.com>
2026-04-08 09:59:41 +0530

    just updated to a optimised version of bubble sorting

--- Sorting.c
+++ Sorting.c
@@ -1,18 +1,24 @@
 #include<stdio.h>
 
 void sort(int arr[], int num){
+    int flag;
     for(int i = 0; i < num - 1; i++){
+        flag = 0;
         for(int j = 0; j < num - i - 1; j++){
-            if(arr[j] < arr[j + 1]){   
+            if(arr[j] > arr[j + 1]){   
                 int temp;
                 temp = arr[j];         
                 arr[j] = arr[j + 1];   
                 arr[j + 1] = temp;
+                flag = 1;
             }
+        }
+        if(flag == 0){
+            break;
         }
     }
 }
-
+// [1,2,3,4,5,6,7,8]
 int main(){
     int arrnew[] = {45, 34, 2, 12, 87, 54, 2, 0};
     int num = 8;
